645d9c061b4334d30d9b854936df716cce4f4bf9
hazel <hazel@chi.swan.ac.uk>
2023-05-09 15:44:18 +0100

    Change project nesting and hopefully .gitignore works

--- DARE-Control/Project Adminv2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_Adminv2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
