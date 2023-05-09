308767c3126906f339442031acb596325d59a41d
Luke-Young20 <74249900+Luke-Young20@users.noreply.github.com>
2023-05-09 15:54:26 +0100

    Moved some files

---
+++ DARE-Control/API Project/Repositories/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Project_Admin.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace Project_Admin.Repositories.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+    }
+}
---
+++ DARE-Control/API Project/Services/Project/IProjectsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Project_Admin.Models;
+using System.Linq.Expressions;
+
+namespace API_Project.Services
+{
+    public interface IProjectsHandler
+    {
+        Task<User> GetUserSettings(int id);
+
+        Task<Projects> GetProjectSettings(int id);
+        Task<User> AddUser(int id);
+        Task<Projects> CreateProjectSettings(Projects model);
+
+        IEnumerable<Project_Admin.Models.Projects> Get(Expression<Func<Project_Admin.Models.Projects, bool>> filter = null, Func<IQueryable<Project_Admin.Models.Projects>, IOrderedQueryable<Project_Admin.Models.Projects>> orderBy = null);
+        Project_Admin.Models.Projects? Get(int id);
+        IEnumerable<Project_Admin.Models.Projects>? GetAll();
+        Task<bool> AddAsync(Project_Admin.Models.Projects ProjectModel);
+        Task<bool> Update(Project_Admin.Models.Projects ProjectModel);
+        Task<bool> Remove(int id);
+        Task<bool> Remove(Project_Admin.Models.Projects p);
+        
+
+    }
+}
