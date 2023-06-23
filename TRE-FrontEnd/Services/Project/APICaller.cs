4c1bb9c7c4db7587c03f16fbc25c691e23e619ad
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2023-06-23 15:15:32 +0100

    Created TRE_Frontend and dependencies downloaded

---
+++ TRE-FrontEnd/Services/Project/APICaller.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+
+namespace TRE_FrontEnd.Services.Project
+{
+    public class APICaller : IAPICaller
+    {
+        public RestClient Client { get; set; }
+
+        public APICaller(string URL)
+        {
+            Client = new RestClient(URL);
+        }
+    }
+
+    public interface IAPICaller
+    {
+        RestClient Client { get; set; }
+    }
+}
