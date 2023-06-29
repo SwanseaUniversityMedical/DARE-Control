73350bb64e452f100ef6a943fc35a071b40c3553
jaybeeelsdon <justin@chi.swan.ac.uk>
2023-06-29 17:12:24 +0100

    New layout

--- API Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace API_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Project Admin/Models/TestModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Project_Admin.Models
-{
-    public class TestModel
-    {
-        public int TestID { get; set; }
-    }
-}
