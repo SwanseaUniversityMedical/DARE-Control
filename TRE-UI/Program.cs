7f190eeda76d6eb90927014904d0cef66ea72d2b
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2023-06-30 16:01:57 +0100

    Gayathrimenon (#34)

---
+++ TRE-UI/Program.cs
@@ -0,0 +1,150 @@
+using Microsoft.IdentityModel.Logging;
+using BL.Services;
+using BL.Models.Services;
+using BL.Models.Settings;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+IdentityModelEventSource.ShowPII = true;
+
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+); ; ;
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
+Log.Logger = CreateSerilogLogger(configuration, environment);
+Log.Information("Dare-FrontEnd logging Start.");
+
+string AppName = typeof(Program).Module.Name.Replace(".dll", "");
+
+// -- authentication here
+var keyCloakSettings = new KeyCloakSettings();
+configuration.Bind(nameof(keyCloakSettings), keyCloakSettings);
+builder.Services.AddSingleton(keyCloakSettings);
+
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+
+
+//add services here
+builder.Services.AddScoped<CustomCookieEvent>();
+
+builder.Services.AddScoped<ITREClientHelper, TREClientHelper>();
+
+builder.Services.AddMvc().AddViewComponentsAsServices();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+    options.OnAppendCookie = cookieContext =>
+        CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+    options.OnDeleteCookie = cookieContext =>
+        CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+Serilog.ILogger CreateSerilogLogger(ConfigurationManager configuration, IWebHostEnvironment environment)
+{
+    var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+    var seqApiKey = configuration["Serilog:SeqApiKey"];
+
+
+
+    return new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .Enrich.WithProperty("ApplicationContext", environment.ApplicationName)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq(seqServerUrl, apiKey: seqApiKey)
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+#region SameSite Cookie Issue - https://community.auth0.com/t/correlation-failed-unknown-location-error-on-chrome-but-not-in-safari/40013/7
+
+void CheckSameSite(HttpContext httpContext, CookieOptions options)
+{
+    if (options.SameSite == SameSiteMode.None)
+    {
+        var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+        //configure cookie policy to omit samesite=none when request is not https
+        if (!httpContext.Request.IsHttps || DisallowsSameSiteNone(userAgent))
+        {
+            options.SameSite = SameSiteMode.Unspecified;
+        }
+    }
+}
+
+
+//  Read comments in https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+bool DisallowsSameSiteNone(string userAgent)
+{
+    // Check if a null or empty string has been passed in, since this
+    // will cause further interrogation of the useragent to fail.
+    if (String.IsNullOrWhiteSpace(userAgent))
+        return false;
+
+    // Cover all iOS based browsers here. This includes:
+    // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+    // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+    // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+    // All of which are broken by SameSite=None, because they use the iOS networking
+    // stack.
+    if (userAgent.Contains("CPU iPhone OS 12") ||
+        userAgent.Contains("iPad; CPU OS 12"))
+    {
+        return true;
+    }
+
+    // Cover Mac OS X based browsers that use the Mac OS networking stack. 
+    // This includes:
+    // - Safari on Mac OS X.
+    // This does not include:
+    // - Chrome on Mac OS X
+    // Because they do not use the Mac OS networking stack.
+    if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+        userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+    {
+        return true;
+    }
+
+    // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+    // and none in this range require it.
+    // Note: this covers some pre-Chromium Edge versions, 
+    // but pre-Chromium Edge does not require SameSite=None.
+    if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+#endregion
