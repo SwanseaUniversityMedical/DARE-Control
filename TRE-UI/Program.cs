5cf22aa35fc9a1a0aeb3441157f7be1af976111a
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2023-07-03 12:53:09 +0100

    Completed setting up basic structure framewrok for TRE-UI and testing

--- TRE-UI/Program.cs
+++ TRE-UI/Program.cs
@@ -48,6 +48,32 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
         CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
 });
 
+builder.Services.AddAuthorization(options =>
+{
+    //options.AddPolicy("admin", policy =>
+    //    policy.RequireClaim("groups", "dare-control-admin")); //MIGHT NEED TO CHANGE LATER
+
+    //probably not needed
+    options.AddPolicy(
+            "admin",
+            policyBuilder => policyBuilder.RequireAssertion(
+                context => context.User.HasClaim(claim =>
+                    claim.Type == "groups"
+                    && claim.Value.Contains("dare-control-admin"))));
+    //options.AddPolicy(
+    //            "company",
+    //            policyBuilder => policyBuilder.RequireAssertion(
+    //                context => context.User.HasClaim(claim =>
+    //                    claim.Type == "groups"
+    //                    && claim.Value.Contains("dare-control-company"))));
+    //options.AddPolicy(
+    //        "user",
+    //        policyBuilder => policyBuilder.RequireAssertion(
+    //            context => context.User.HasClaim(claim =>
+    //                claim.Type == "groups"
+    //                && claim.Value.Contains("dare-control-user"))));
+});
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
