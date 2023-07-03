8b3e6a34614df2899ce6f32b3eae45f384e370e9
Simon Thompson <simon@chi.swan.ac.uk>
2023-07-03 21:40:24 +0100

    everything moved around

---
+++ src/OLD-bl/Models/DTO/ProjectUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace BL.Models.DTO
+{
+    public class ProjectUser
+    {
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        [JsonIgnore]
+        public IEnumerable<SelectListItem>? ProjectItemList { get; set; }
+        [JsonIgnore]
+        public IEnumerable<SelectListItem>? UserItemList { get; set; }
+    }
+}
---
+++ src/OLD-bl/Models/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿using BL.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Models
+{
+    public class Endpoint
+    {
+        public int Id { get; set; }
+
+        
+        public virtual List<Project> Projects { get; set; }
+
+        public string Name { get; set; }
+
+        public string FormData { get; set; }
+
+        public virtual List<Submission> Submissions { get; set; }
+
+
+    }
+}
