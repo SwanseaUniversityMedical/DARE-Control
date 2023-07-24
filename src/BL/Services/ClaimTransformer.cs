1c48ebe6a5da01e0aafb41983f28bd9f2cee79f9
Simon Thompson <simon@chi.swan.ac.uk>
2023-07-24 08:20:15 +0100

    minoir: all the code needed

--- src/BL/Services/ClaimTransformer.cs
+++ src/BL/Services/ClaimTransformer.cs
@@ -14,6 +14,21 @@ namespace BL.Services
         public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
         {
             ClaimsIdentity claimsIdentity = principal.Identity as ClaimsIdentity;
+            
+            // flatten realm_access because Microsoft identity model doesn't support nested claims
+            // by map it to Microsoft identity model, because automatic JWT bearer token mapping already processed here
+            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "realm_access"))
+            {
+                var realmAccessClaim = claimsIdentity.FindFirst((claim) => claim.Type == "realm_access");
+                var realmAccessAsDict = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(realmAccessClaim.Value);
+                if (realmAccessAsDict["roles"] != null)
+                {
+                    foreach (var role in realmAccessAsDict["roles"])
+                    {
+                        claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
+                    }
+                }
+            }
 
             return Task.FromResult(principal);
         }
