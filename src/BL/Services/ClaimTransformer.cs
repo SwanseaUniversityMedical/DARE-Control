74064e50dd07f206407edc03f84c85f812df21a6
Simon Thompson <simon@chi.swan.ac.uk>
2023-07-25 12:38:49 +0100

    feat: force rebuild

--- src/BL/Services/ClaimTransformer.cs
+++ src/BL/Services/ClaimTransformer.cs
@@ -9,31 +9,6 @@ using System.Threading.Tasks;
 
 namespace BL.Services
 {
-    public class dummytraformThing : IClaimsTransformation
-    {
-        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
-        {
-            ClaimsIdentity claimsIdentity = principal.Identity as ClaimsIdentity;
-            
-            // flatten realm_access because Microsoft identity model doesn't support nested claims
-            // by map it to Microsoft identity model, because automatic JWT bearer token mapping already processed here
-            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "realm_access"))
-            {
-                var realmAccessClaim = claimsIdentity.FindFirst((claim) => claim.Type == "realm_access");
-                var realmAccessAsDict = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(realmAccessClaim.Value);
-                if (realmAccessAsDict["roles"] != null)
-                {
-                    foreach (var role in realmAccessAsDict["roles"])
-                    {
-                        claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
-                    }
-                }
-            }
-
-            return Task.FromResult(principal);
-        }
-    }
-
     public class ClaimsTransformerBL : IClaimsTransformation
     {
         public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
