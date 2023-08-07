347d8d27717e351047a6da6e340063e01a770f06
jaybeeelsdon <justin@chi.swan.ac.uk>
2023-08-07 18:21:26 +0100

    asdad

--- src/BL/Models/Settings/FormIOSettings.cs
+++ src/BL/Models/Settings/FormIOSettings.cs
@@ -9,6 +9,7 @@ namespace BL.Models.Settings
     public interface IFormIOSettings
     {
         string EndpointForm { get; set; }
+        string EndpointForm2 { get; set; }
         string ProjectForm { get; set; }
         bool UseInternal { get; set; }
         string UserForm { get; set; }
