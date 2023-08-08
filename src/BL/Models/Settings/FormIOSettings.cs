3ba8addfe350103b74a32f71ad9f2c13d10fcf17
jaybeeelsdon <justin@chi.swan.ac.uk>
2023-08-08 10:07:43 +0100

    Removed debug

--- src/BL/Models/Settings/FormIOSettings.cs
+++ src/BL/Models/Settings/FormIOSettings.cs
@@ -9,7 +9,7 @@ namespace BL.Models.Settings
     public interface IFormIOSettings
     {
         string EndpointForm { get; set; }
-        string EndpointForm2 { get; set; }
+        
         string ProjectForm { get; set; }
         bool UseInternal { get; set; }
         string UserForm { get; set; }
@@ -20,13 +20,13 @@ namespace BL.Models.Settings
         private string projectForm;
         private string userForm;
         private string endpointForm;
-        private string endpointForm2;
+        
         private bool useInternal = false;
         private string baseURL = "";
         const string in_projectForm="/formio/project.json";
         const string in_userForm="/formio/user.json";
         const string in_endpointForm="/formio/endpoint.json";
-        const string in_endpointForm2 = "/formio/endpoint2.json";
+        
 
 
         public bool UseInternal { get => useInternal; set => useInternal = value; }
@@ -36,11 +36,7 @@ namespace BL.Models.Settings
             set => endpointForm = value;
         }
 
-        public string EndpointForm2
-        {
-            get => useInternal == true ? in_endpointForm2 : endpointForm2;
-            set => endpointForm2 = value;
-        }
+        
         public string UserForm
         {
             get => useInternal==true ?  in_userForm : userForm;
