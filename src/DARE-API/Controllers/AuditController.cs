00817ff44e3154849d273e55db5f2268eba7e68e
Patricia <patricia.swan.ac.uk>
2023-09-08 15:32:21 +0100

    added-auditlogclass in view modelad and audit controllers in dare api

---
+++ src/DARE-API/Controllers/AuditController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DARE_API.Controllers
+{
+    public class AuditController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
