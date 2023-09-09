9638b28160ac46b69090a60ea73f3b0276e0db24
Simon Thompson <simon@chi.swan.ac.uk>
2023-09-09 18:44:42 +0100

    start of placeholders and more error checking

--- src/TREAgent/DoWork.cs
+++ src/TREAgent/DoWork.cs
@@ -8,6 +8,7 @@ using BL.Rabbit;
 using Microsoft.Extensions.DependencyInjection;
 using EasyNetQ;
 using Newtonsoft.Json;
+using Serilog;
 using TREAgent.Services;
 
 namespace TREAgent
@@ -20,75 +21,138 @@ namespace TREAgent
     public class DoWork : IDoWork
     {
         private readonly IServiceProvider _serviceProvider;
-
-        
-
-
         public DoWork(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         
         }
 
+        // Method executed upon hangfire job
         public void Execute()
         {
-
-           
+            // control use of dependency injection
             using (var scope = _serviceProvider.CreateScope())
             {
-                var rabbit = scope.ServiceProvider.GetRequiredService<IBus>(); ;
-                var exch = rabbit.Advanced.ExchangeDeclare(ExchangeConstants.Main, "topic");
+                // OPTIONS
+                // TODO get these from somewhere
+                
+                var useRabbit = true;
+                var useHutch = true;
+                var useTESK = true;
+
+
+                // Get list of submissions
                 var treApi = scope.ServiceProvider.GetRequiredService<ITreClientWithoutTokenHelper>();
-                
-                
-                var subs = treApi.CallAPIWithoutModel<List<Submission>>("/api/Submission/GetWaitingSubmissionsForTre").Result;
+                var listOfSubmissions = treApi.CallAPIWithoutModel<List<Submission>>("/api/Submission/GetWaitingSubmissionsForTre").Result;
 
 
-               
-                
-                foreach (var submission in subs)
+                foreach (var aSubmission in listOfSubmissions)
                 {
-                    var paramlist = new Dictionary<string, string>
-                    {
-                        { "projectId", submission.Project.Id.ToString() },
-                        { "userId", submission.SubmittedBy.Id.ToString() }
+                    Log.Information("Submission: {submission}", aSubmission);
 
-                    };
-
-                    if (!treApi.CallAPIWithoutModel<BoolReturn>("/api/Submission/IsUserApprovedOnProject", paramlist)
-                            .Result.Result)
+                    var paramlist = new Dictionary<string, string>();
+                    try
                     {
+                        paramlist.Add("projectId", aSubmission.Project.Id.ToString());
+                        paramlist.Add("userId", aSubmission.SubmittedBy.Id.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error("Submission does not have project and/or user");
+                    }
+                    
+                    // Check user is allowed ont he project
+                    if ( paramlist.Count==2 && !treApi.CallAPIWithoutModel<BoolReturn>("/api/Submission/IsUserApprovedOnProject", paramlist).Result.Result)
+                    {
+                        Log.Error("User/project {details} is not value for this submission {submission}", paramlist, aSubmission);
+                        // record error with submission layer
                         var result = treApi.CallAPIWithoutModel<APIReturn>("/api/Submission/UpdateStatusForTre",
                             new Dictionary<string, string>()
                             {
-                                { "tesId", submission.TesId }, { "statusType", StatusType.InvalidUser.ToString() },
+                                { "tesId", aSubmission.TesId }, { "statusType", StatusType.InvalidUser.ToString() },
                                 { "description", "" }
                             }).Result;
                     }
                     else
                     {
+                        // The TES message
+                        var tesMessage = JsonConvert.DeserializeObject<TesTask>(aSubmission.TesJson);
+                        var processedOK = true;
 
-
-                        //TODO: Check crate format
-                        //TODO: Call API or rabbit for testing (Only dump tesString)
-
-                        var tes = JsonConvert.DeserializeObject<TesTask>(submission.TesJson);
-                        rabbit.Advanced.Publish(exch, RoutingConstants.Subs, false, new Message<TesTask>(tes));
-                        var result = treApi.CallAPIWithoutModel<APIReturn>("/api/Submission/UpdateStatusForTre",
-                            new Dictionary<string, string>()
+                        // **************  SEND TO RABBIT
+                        if (useRabbit)
+                        {
+                            try
                             {
-                                { "tesId", submission.TesId }, { "statusType", StatusType.TransferredToPod.ToString() },
-                                { "description", "" }
-                            }).Result;
-                        
+                                IBus rabbit = scope.ServiceProvider.GetRequiredService<IBus>();
+                                EasyNetQ.Topology.Exchange exchangeObject = rabbit.Advanced.ExchangeDeclare(ExchangeConstants.Main, "topic");
+                                rabbit.Advanced.Publish(exchangeObject, RoutingConstants.Subs, false, new Message<TesTask>(tesMessage));
+                            }
+                            catch (Exception e)
+                            {
+                               Log.Error("Send rabbit failed : {message}",e.Message);
+                               processedOK = false;
+                            }
+                        }
+
+                        // **************  SEND TO HUTCH
+                        if (useHutch)
+                        {
+                            // TODO for rest API
+                            try
+                            {
+
+                            }
+                            catch (Exception e)
+                            {
+                                Log.Error("Send HUTCH failed : {message}", e.Message);
+                                processedOK = false;
+                            }
+                        }
+
+                        // **************  SEND TO TESK
+                        if (useTESK)
+                        {
+                            // TODO RESTAPI and hangfire job to follow up
+                            try
+                            {
+
+                            }
+                            catch (Exception e)
+                            {
+                                Log.Error("Send TESK failed : {message}", e.Message);
+                                processedOK = false;
+                            }
+
+                        }
+
+                        // **************  TELL SUBMISSION LAYER WE DONE
+                        if (processedOK)
+                        {
+                            try
+                            {
+                                var result = treApi.CallAPIWithoutModel<APIReturn>("/api/Submission/UpdateStatusForTre",
+                                    new Dictionary<string, string>()
+                                    {
+                                        { "tesId", aSubmission.TesId },
+                                        { "statusType", StatusType.TransferredToPod.ToString() },
+                                        { "description", "" }
+                                    }).Result;
+                            }
+                            catch (Exception e)
+                            {
+                                Log.Error("Send record outcome to submission layer : {message}", e.Message);
+                                processedOK = false;
+                            }
+                         
+                        }
+
                     }
-
                 }
-
-
             }
-            // Use the app settings value here
-
+            
         }
+
+   
     }
 }
