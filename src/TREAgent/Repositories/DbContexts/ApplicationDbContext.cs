54d5e624adef7577ca25ca1cebe8e53a4437af53
Simon Thompson <simon@chi.swan.ac.uk>
2023-09-12 11:46:09 +0100

    Agent refactor (#118)
    Co-authored-by: jaybeeelsdon <justin@chi.swan.ac.uk>

---
+++ src/TREAgent/Repositories/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TREAgent.Repositories.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("agent");
+        }
+
+        public DbSet<TESKstatus> TESK_Status { get; set; }
+        public DbSet<TeskAudit> TESK_Audit { get; set; }
+
+    }
+}
