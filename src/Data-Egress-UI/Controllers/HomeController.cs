84dfe9d1d3ec6d73ff5ea68239b2201bf7fc4b2c
Patricia <patricia.swan.ac.uk>
2023-09-22 14:38:07 +0100

    feat: added the DataEgressClienthelper

--- src/Data-Egress-UI/Controllers/HomeController.cs
+++ src/Data-Egress-UI/Controllers/HomeController.cs
@@ -1,32 +1,61 @@
 ﻿using Data_Egress_UI.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace Data_Egress_UI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITREClientHelper _treClientHelper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITREClientHelper trehelper)
         {
             _logger = logger;
+            _treClientHelper = trehelper;
         }
 
-        public IActionResult Index()
+        public IActionResult LoginAfterTokenExpired()
         {
+            return new SignOutResult(new[]
+            {
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            }, new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("Login", "Home")
+            });
+        }
+
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Login", "Home");
+        }
+        [Authorize]
+        public IActionResult Logout()
+        {
+            return new SignOutResult(new[]
+            {
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            }, new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("Login", "Home")
+            });
+        }
+        public async Task<IActionResult> AccessDenied(string ReturnUrl)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
