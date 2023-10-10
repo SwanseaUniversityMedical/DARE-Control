851993ea8a4863bc6964e14f42d3722b304f4574
Luke-Young20 <74249900+Luke-Young20@users.noreply.github.com>
2023-10-10 18:57:12 +0100

    Showing numbers on the submissions and projects

--- src/DARE-FrontEnd/Controllers/HomeController.cs
+++ src/DARE-FrontEnd/Controllers/HomeController.cs
@@ -5,22 +5,33 @@ using Microsoft.AspNetCore.Authorization;
 
 using Microsoft.AspNetCore.Mvc;
 using BL.Services;
+using BL.Models.ViewModels;
+using NuGet.Protocol;
 
 namespace DARE_FrontEnd.Controllers
 {
-    
+
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        
 
-        
+        private readonly IDareClientHelper _clientHelper;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IDareClientHelper client, IConfiguration configuration)
+        {
+            _clientHelper = client;
+            _configuration = configuration;
+        }
 
         public IActionResult Index()
         {
+            var getAllProj = _clientHelper.CallAPIWithoutModel< List<Project>>("/api/Project/GetAllProjects").Result;
+            ViewBag.getAllProj = getAllProj.Count;
+            var getAllSubs = _clientHelper.CallAPIWithoutModel<List<Submission>>("/api/Submission/GetAllSubmissions").Result;
+            ViewBag.getAllSubs = getAllSubs.Count;
             return View();
         }
-
         public IActionResult TermsAndConditions()
         {
             return View();
@@ -29,7 +40,7 @@ namespace DARE_FrontEnd.Controllers
         {
             return View();
         }
-
     }
+
 }
 
