6024b0cfdd066638a16d0e45a39e90c0eceed1b1
hazel <hazel@chi.swan.ac.uk>
2023-10-11 15:05:26 +0100

    configuration changes

--- src/Tre-Hasura/Program.cs
+++ src/Tre-Hasura/Program.cs
@@ -56,8 +56,7 @@ IConfiguration GetConfiguration()
 
     var builder = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddJsonFile($"appsettings.development.json", optional: true, reloadOnChange: true)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)       
         .AddEnvironmentVariables();
 
     return builder.Build();
