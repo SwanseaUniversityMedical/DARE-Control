3f02a32b2addba94715e1297382c2d0d9f3bc32d
Simon Thompson <simon@chi.swan.ac.uk>
2023-10-14 23:27:51 +0100

    feat: Fix ef issue (#224)

--- src/TRE-API/Controllers/SubmissionCredentialsController.cs
+++ src/TRE-API/Controllers/SubmissionCredentialsController.cs
@@ -54,38 +54,35 @@ namespace TRE_API.Controllers
             try
             {
                 creds.Valid = true;
-                var token = await _keycloakTokenHelper.GetTokenForUser(creds.UserName,
-                    creds.PasswordEnc, "dare-tre-admin");
+                var token = await _keycloakTokenHelper.GetTokenForUser(creds.UserName, creds.PasswordEnc, "dare-tre-admin");
                 if (string.IsNullOrWhiteSpace(token))
                 {
                     creds.Valid = false;
                     return creds;
                 }
-                
-                var add = true;
-                var dbcred = _DbContext.KeycloakCredentials.FirstOrDefault(x => x.CredentialType == CredentialType.Submission);
-                if (dbcred != null)
-                {
-                    creds.Id = dbcred.Id;
-                    creds.CredentialType = CredentialType.Submission;
-                    add = false;
-                }
-
-                creds.PasswordEnc = _encDecHelper.Encrypt(creds.PasswordEnc);
-                if (add)
-                {
-                    _DbContext.KeycloakCredentials.Add(creds);
-                    
-                }
                 else
                 {
-                    _DbContext.KeycloakCredentials.Update(creds);
-                }
-                
-                await _DbContext.SaveChangesAsync();
 
-                Log.Information("{Function} Credentials Successfully update", "UpdateCredentials");
-                return creds;
+                    var dbcred =
+                        _DbContext.KeycloakCredentials.FirstOrDefault(
+                            x => x.CredentialType == CredentialType.Submission);
+                    if (dbcred == null)
+                        dbcred = new KeycloakCredentials();
+
+                    dbcred.CredentialType = CredentialType.Submission;
+                    dbcred.UserName = creds.UserName;
+                    dbcred.PasswordEnc = _encDecHelper.Encrypt(creds.PasswordEnc);
+
+                    if (dbcred.Id == 0)
+                        _DbContext.KeycloakCredentials.Add(dbcred);
+                    else
+                        _DbContext.KeycloakCredentials.Update(dbcred);
+
+                    await _DbContext.SaveChangesAsync();
+
+                    Log.Information("{Function} Credentials Successfully update", "UpdateCredentials");
+                    return creds;
+                }
             }
             catch (Exception ex)
             {
