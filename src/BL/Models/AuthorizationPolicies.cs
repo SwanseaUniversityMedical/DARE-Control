278911bb0a3064079ec1841728bbc02600ce5e90
Patricia <patricia.swan.ac.uk>
2023-10-24 10:04:11 +0100

    added AuthorizationPolicies class to BL

---
+++ src/BL/Models/AuthorizationPolicies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BL.Models
+{
+    public static class AuthorizationPolicies
+    {
+        public static AuthorizationPolicy GetUserAllowedPolicy()
+        {
+            var policyBuilder = new AuthorizationPolicyBuilder(); 
+            //add other policy requirements here
+
+            policyBuilder.RequireClaim("user_in_tre");
+            policyBuilder.RequireClaim("allow");
+            policyBuilder.RequireClaim("project_allow");
+            return policyBuilder.Build(); 
+        }
+    }
+}
