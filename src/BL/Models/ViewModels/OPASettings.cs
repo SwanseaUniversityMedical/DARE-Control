9bc0d973afbb52a126900b667f6b1afa8b28b94f
Patricia <patricia.swan.ac.uk>
2023-11-01 17:00:34 +0000

    added Expiry Delay Minutes in app config and opa service

--- src/BL/Models/ViewModels/OPASettings.cs
+++ src/BL/Models/ViewModels/OPASettings.cs
@@ -4,7 +4,9 @@ namespace BL.Models.ViewModels
     public class OPASettings
     {
         public string? OPAUrl { get; set; }
-      
+        public double ExpiryDelayMinutes { get; set; }
+        public bool UseRealExpiry { get; set; }
+
     }
 }
 
