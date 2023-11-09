64b8fee10ebe01e86d8b41fe55d6de64f0f2a57d
Patricia <patricia.swan.ac.uk>
2023-11-09 09:50:16 +0000

    removed instances on TREUI

--- src/TRE-UI/Controllers/HomeController.cs
+++ src/TRE-UI/Controllers/HomeController.cs
@@ -41,20 +41,7 @@ namespace TRE_UI.Controllers
            return RedirectToAction("GetAllProjects", "Approval");
             //return View();
         }
-        //[HttpPost]
-        //public IActionResult IndexAsync(string SearchString)
-        //{
-        //    List<SearchResult> results = SearchData(SearchString);
-        //    ViewBag.SearchResults = results;
-        //    return View();
-        //}
-        ////private helpers
-        //private List<SearchResult> SearchData(string SearchString)
-        //{
-        //    List<SearchResult> results = new List<SearchResult>();
-        //      return results;
-
-        //}
+       
         public IActionResult LoginAfterTokenExpired()
         {
             return new SignOutResult(new[]
