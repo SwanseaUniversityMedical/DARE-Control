a7ee0be94cd2b834a367f30f1185cfc5118c77ae
John.Vaughan <john.vaughan@chi.swan.ac.uk>
2023-11-14 14:51:03 +0000

    fate/bucket2

--- src/BL/Services/MinioHelper.cs
+++ src/BL/Services/MinioHelper.cs
@@ -311,16 +311,23 @@ namespace BL.Services
             var signer = new AWS4RequestSigner(_minioSettings.AccessKey, _minioSettings.SecretKey);
 
             var content = new StringContent(@"{
-                ""Version"": ""2012-10-17"",
-                ""Statement"": [
-                    {
-                        ""Effect"": ""Allow"",
-                        ""Principal"": ""*"",
-                        ""Action"": ""s3:GetObject"",
-                        ""Resource"": ""arn:aws:s3:::" + bucketName + @"/*""
-                    }
-                ]
-            }");
+            ""Version"": ""2012-10-17"",
+            ""Statement"": [
+                {
+                    ""Effect"": ""Allow"",
+                    ""Principal"": ""*"",
+                    ""Action"": [
+                        ""s3:GetObject"",
+                        ""s3:PutObject"",
+                        ""s3:ListBucket""
+                    ],
+                    ""Resource"": [
+                        ""arn:aws:s3:::" + bucketName + @"/*"",
+                        ""arn: aws:s3:::" + bucketName + @"
+                    ]
+                }
+            ]
+        }");
 
             var request = new HttpRequestMessage
             {
