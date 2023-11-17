1c6c5f556bdb2db5280f920adcaa7acd7ae48578
John.Vaughan <john.vaughan@chi.swan.ac.uk>
2023-11-17 15:14:54 +0000

    no need here

--- src/TRE-API/Controllers/SubmissionController.cs
+++ src/TRE-API/Controllers/SubmissionController.cs
@@ -299,8 +299,8 @@ namespace TRE_API.Controllers
                         Log.Information($"EgressResults with File.Approved > {File.Approved} File.FileName > {File.FileName} ");
                         if (File.Approved)
                         {
-                            var source = _minioTreHelper.GetCopyObject(review.OutputBucket, $"/{review.SubId}/" + File.FileName);
-                            var resultcopy = _minioSubHelper.CopyObjectToDestination(bucket.Bucket, $"/{review.SubId}/" + File.FileName, source.Result).Result;
+                            var source = _minioTreHelper.GetCopyObject(review.OutputBucket,  File.FileName);
+                            var resultcopy = _minioSubHelper.CopyObjectToDestination(bucket.Bucket,  File.FileName, source.Result).Result;
                         }
                     }
                 }
