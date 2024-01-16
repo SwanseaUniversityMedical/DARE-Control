b55e1ba76c50137adef3edfc2d103f424b47139c
jaybeeelsdon <justin@chi.swan.ac.uk>
2024-01-16 12:55:54 +0000

    fix: try with missing bit

--- src/TRE-UI/Program.cs
+++ src/TRE-UI/Program.cs
@@ -261,7 +261,7 @@ else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
+    app.UseHsts();
 }
 //app.UseForwardedHeaders(new ForwardedHeadersOptions
 //{
