882f811eb16d26edffc980d3eac038738241fe14
John.Vaughan <john.vaughan@chi.swan.ac.uk>
2024-04-16 11:49:38 +0100

    SCHEMA

--- src/Tre-SQL-Trino/entrypoint.sh
+++ src/Tre-SQL-Trino/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if required environment variables are provided
-if [[ -z "$TRINO_SERVER_URL" || -z "$SQL_STATEMENT" || -z "$ACCESS_TOKEN" || -z "$USER_NAME"  ]]; then
-    echo "Please provide TRINO_SERVER_URL, ACCESS_TOKEN, SQL_STATEMENT and USER_NAME environment variables ."
+if [[ -z "$TRINO_SERVER_URL" || -z "$SQL_STATEMENT" || -z "$ACCESS_TOKEN" || -z "$USER_NAME" || -z "$SCHEMA"  ]]; then
+    echo "Please provide TRINO_SERVER_URL, ACCESS_TOKEN, SCHEMA, SQL_STATEMENT and USER_NAME environment variables ."
     exit 1
 fi
 
 
 # Execute Trino CLI command to run SQL statement and output CSV
-trino --server "$TRINO_SERVER_URL" --execute "$SQL_STATEMENT" --access-token "$ACCESS_TOKEN" --user "$USER_NAME" --insecure --output-format CSV > ~/result.csv
+trino --server "$TRINO_SERVER_URL" --execute "$SQL_STATEMENT" --access-token "$ACCESS_TOKEN" --user "$USER_NAME" --schema "$SCHEMA" --insecure --output-format CSV > ~/result.csv
 
 ls -la ~/
 
