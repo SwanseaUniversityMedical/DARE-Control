8f541a3b474b19984c998d54afa296fc4cc1684b
John.Vaughan <john.vaughan@chi.swan.ac.uk>
2024-04-22 14:59:34 +0100

    Revert "fix"
    This reverts commit 17c5f77dfeb5c425eaeccbbc9559d889166ee668.

--- src/TRE-API/Services/DareClientWithoutTokenHelper.cs
+++ src/TRE-API/Services/DareClientWithoutTokenHelper.cs
@@ -22,7 +22,7 @@ namespace TRE_API.Services
             {
                 _username = creds.UserName;
                 _password = encDec.Decrypt(creds.PasswordEnc);
-                _requiredRole = "dare-tre-admin";
+                _requiredRole = "dare-control-admin";
             }
         }
 
