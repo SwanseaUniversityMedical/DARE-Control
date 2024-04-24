9148e20a7014c591194a69be8ed76266b924eeb8
Simon Thompson <simon@chi.swan.ac.uk>
2024-04-24 21:37:08 +0100

    fix: set output location

--- src/Tre-SQL-Trino/entrypoint.sh
+++ src/Tre-SQL-Trino/entrypoint.sh
@@ -1,14 +1,10 @@
-!/bin/bash
+#!/bin/bash
 
 # Check if required environment variables are provided
-if [[ -z "$TRINO_SERVER_URL" || -z "$SQL_STATEMENT" ]]; then
-    echo "Please provide TRINO_SERVER_URL and SQL_STATEMENT environment variables."
+if [[ -z "$TRINO_SERVER_URL" || -z "$SQL_STATEMENT" || -z "$LOCATION" ]]; then
+    echo "Please provide TRINO_SERVER_URL, SQL_STATEMENT and LOCATION environment variables."
     exit 1
 fi
 
-
 # Execute Trino CLI command to run SQL statement and output CSV
-trino --server "$TRINO_SERVER_URL" --execute "$SQL_STATEMENT" --output-format CSV > ~/result.csv
-
-ls -la ~/
-
+trino --server "$TRINO_SERVER_URL" --execute "$SQL_STATEMENT" --output-format CSV > $LOCATION
