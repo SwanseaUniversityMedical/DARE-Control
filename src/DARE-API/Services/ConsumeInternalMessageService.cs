5c21c62e012532b40103785eff2133f8b27f318e
jaybeeelsdon <justin@chi.swan.ac.uk>
2024-07-25 16:18:23 +0100

    fix: idiot (#767)

--- src/DARE-API/Services/ConsumeInternalMessageService.cs
+++ src/DARE-API/Services/ConsumeInternalMessageService.cs
@@ -95,9 +95,9 @@ namespace DARE_API.Services
                     {
                         
                         string fileName = Path.GetFileName(uri.LocalPath);
-                        Log.Information("{Function} Full file loc {File}, Incoming URL {URL}, our minio {Minio}", "Process", fileName, uri.Host + ":" + uri.Port, _minioSettings.AdminConsole);
+                        Log.Information("{Function} Full file loc {File}, Incoming URL {URL}, our minio {Minio}", "Process", fileName, uri.Scheme + ":\\" + uri.Host + ":" + uri.Port, _minioSettings.AdminConsole);
                         messageMQ.Key = fileName;
-                        if (uri.Host + ":" + uri.Port != _minioSettings.AdminConsole)
+                        if (uri.Scheme + ":\\" + uri.Host + ":" + uri.Port != _minioSettings.AdminConsole)
                         {
                             Log.Information("{Function} Copying external", "Process");
                             _minioHelper.RabbitExternalObject(messageMQ);
