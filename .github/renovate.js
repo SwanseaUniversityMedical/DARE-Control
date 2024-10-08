09055c5b788ca699dc4663892b177acdbf839dd2
Joss Whittle <joss.whittle@gmail.com>
2024-10-09 00:14:43 +0100

    test: tweak config

--- .github/renovate.js
+++ .github/renovate.js
@@ -47,8 +47,8 @@ module.exports = {
 
   branchPrefix: "upgrade/",
   //separateMajorMinor: false,
-  groupName: "all dependencies",
-  groupSlug: "all",
+  //groupName: "all dependencies",
+  //groupSlug: "all",
   
   packageRules: [
     {
