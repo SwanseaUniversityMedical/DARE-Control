d4a4eac127edbf8a22f433803d719cce802d6966
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2025-06-18 13:19:46 +0100

    Created base json input model with decisionID

---
+++ src/TRE-API/Models/DmnRequest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TRE_API.Models
+{
+    public class DmnRequest
+    {
+
+        public string DecisionId {  get; set; }
+
+        public Dictionary<string, object> Variables { get; set; } = new();
+    }
+}
---
+++ src/TRE-API/Models/DmnResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TRE_API.Models
+{
+    public class DmnResponse
+    {
+
+        public string DecisionId { get; set; }
+
+        public Dictionary<string, object> Result { get; set; } = new();
+    }
+}
