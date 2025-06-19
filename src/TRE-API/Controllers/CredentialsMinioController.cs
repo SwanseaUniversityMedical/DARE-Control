08dfefbe130e65640eca9da5f2cb5af851f327ca
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2025-06-19 12:49:19 +0100

    Controller API method meant for testing, can move it around to the correct controller

--- src/TRE-API/Controllers/CredentialsMinioController.cs
+++ src/TRE-API/Controllers/CredentialsMinioController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TRE_API.Models;
+using TRE_API.Services;
 
 namespace TRE_API.Controllers
 {
     public class CredentialsMinioController : Controller
     {
-        public IActionResult Index()
+        private readonly IZeebeDmnService _zeebeDmnService;
+        
+        public CredentialsMinioController(IZeebeDmnService zeebeDmnService)
         {
-            return View();
+            _zeebeDmnService = zeebeDmnService;
         }
+
+        [HttpPost("evaluateDMN")]
+        public async Task<IActionResult> EvaluateDecisionModel([FromBody] DmnRequest dmnRequest)
+        {
+            try
+            {
+                var result = await _zeebeDmnService.EvaluateDecisionModelAsync(dmnRequest);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }       
+
     }
 }
