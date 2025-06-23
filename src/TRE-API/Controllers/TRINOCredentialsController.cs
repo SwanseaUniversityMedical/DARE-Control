4a329dbd4011d99e1631a3afe9bafcc570573f41
Matthew <matthew.wong@chi.swan.ac.uk>
2025-06-23 13:06:05 +0100

    Added TRINO Credentials Controller skeleton.

--- src/TRE-API/Controllers/TRINOCredentialsController.cs
+++ src/TRE-API/Controllers/TRINOCredentialsController.cs
@@ -1,25 +1,23 @@
-﻿using BL.Models;
-using BL.Models.APISimpleTypeReturns;
+﻿using BL.Models.APISimpleTypeReturns;
+using BL.Models;
 using BL.Models.Settings;
-using BL.Services;
 using Microsoft.AspNetCore.Authorization;
+using BL.Services;
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
 using TRE_API.Repositories.DbContexts;
 using TRE_API.Services;
+
 namespace TRE_API.Controllers
 {
     [Route("api/[controller]")]
-
     [ApiController]
-    public class TRECredentialsController : Controller
+    public class TRINOCredentialsController : Controller
     {
-
         private readonly ApplicationDbContext _DbContext;
         private readonly IEncDecHelper _encDecHelper;
         private readonly KeycloakTokenHelper _keycloakTokenHelper;
 
-        public TRECredentialsController(ApplicationDbContext applicationDbContext, IEncDecHelper encDec, TreKeyCloakSettings keycloakSettings)
+        public TRINOCredentialsController (ApplicationDbContext applicationDbContext, IEncDecHelper encDec, TreKeyCloakSettings keycloakSettings)
         {
             _encDecHelper = encDec;
             _DbContext = applicationDbContext;
@@ -28,18 +26,10 @@ namespace TRE_API.Controllers
         }
 
         [Authorize(Roles = "dare-tre-admin")]
-        [HttpGet("CheckCredentialsAreValid")]
+        [HttpGet("validate")]
         public async Task<BoolReturn> CheckCredentialsAreValidAsync()
         {
-            return await ControllerHelpers.CheckCredentialsAreValid(_keycloakTokenHelper, _encDecHelper, _DbContext, CredentialType.Tre);
-        }
-        [Authorize(Roles = "dare-tre-admin")]
-        [HttpPost("UpdateCredentials")]
-
-        public async Task<KeycloakCredentials> UpdateCredentials(KeycloakCredentials creds)
-        {
-            creds = await ControllerHelpers.UpdateCredentials(creds, _keycloakTokenHelper, _DbContext, _encDecHelper, CredentialType.Tre, "dare-tre-admin");
-            return creds;
+            return await ControllerHelpers.CheckCredentialsAreValid(_keycloakTokenHelper, _encDecHelper, _DbContext, CredentialType.Submission);
         }
     }
 }
