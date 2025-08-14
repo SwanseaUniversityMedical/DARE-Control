418347ac89386d2524fd3158cc8ecadcba3b7048
Gayathri Menon <42384783+Gayathri-Menon@users.noreply.github.com>
2025-08-14 09:41:13 +0100

    feat: User management service for trino, camunda changes to bpmn diagram, handler for User creation (#956)
    * Docker-compose config to create OU named Users
    
    * OpenLDAP config settings
    
    * Trino User management service using openldap
    
    * Config changes to correctly receive the config values from appsettings and DI for new user management service
    
    * Correct way to config LdapSettings in Program.cs for DI
    
    * New Trino Handler for user creation using OpenLDAP
    
    * More code changes to make openLDAP connection work, also set create users correctly

---
+++ src/Tre-Camunda/Services/ILdapUserManagementService.cs
@@ -0,0 +1,11 @@
+﻿using BL.Models;
+
+namespace Tre_Camunda.Services
+{
+    public interface ILdapUserManagementService
+    {
+        Task<UserCreationResult> CreateUserAsync(CreateUserRequest request);
+
+        Task<UserCreationResult> DeleteUserAsync(string username);        
+    }
+}
