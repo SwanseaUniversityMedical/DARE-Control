deeb67f51605fe2d6c3fe059f478563a3d01a5c5
John-Vaughan <114492829+John-Vaughan@users.noreply.github.com>
2025-10-16 09:40:12 +0100

    patch: adds log for _minioSubHelper.GetCopyObject in DoAgentWork (#996)
    * adds log
    
    * fix
    
    * try catch
    
    * more logs
    
    * no NRE
    
    * BadRequest stuff
    
    * no Streamable?
    
    * remove logs
    
    * fixes error not showing  GraphQL TES Wizard

--- src/TRE-API/DoAgentWork.cs
+++ src/TRE-API/DoAgentWork.cs
@@ -5,6 +5,7 @@ using BL.Models.Tes;
 using BL.Models.ViewModels;
 using BL.Rabbit;
 using BL.Services;
+using Build.Security.AspNetCore.Middleware.Dto;
 using EasyNetQ;
 using Hangfire;
 using Microsoft.FeatureManagement;
@@ -149,10 +150,11 @@ namespace TRE_API
                 string responseBody = response.Content.ReadAsStringAsync().Result;
                 Log.Error("{Function} Request failed with status code: {Code} {responseBody}", "CreateTESK", response.StatusCode, responseBody);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Log.Error("{Function} Request failed with status code: {Code}", "CreateTESK", response.StatusCode);
             }
-            
+
 
 
             return "";
@@ -221,7 +223,7 @@ namespace TRE_API
                         if (shouldReport || (status.state == "COMPLETE" || status.state == "EXECUTOR_ERROR" ||
                                              status.state == "SYSTEM_ERROR"))
                         {
-                            Log.Information("{Function} *** status change *** {State} {name} {description}", "CheckTES", status.state,status.name ,  status.description);
+                            Log.Information("{Function} *** status change *** {State} {name} {description}", "CheckTES", status.state, status.name, status.description);
 
 
                             // send update
@@ -285,7 +287,7 @@ namespace TRE_API
                                 }
 
                                 APIReturn? result = null;
- 
+
 
                                 if (status.state == "COMPLETE")
                                 {
@@ -390,7 +392,7 @@ namespace TRE_API
                 if (await _features.IsEnabledAsync(FeatureFlags.DemoAllInOne))
                 {
                     Log.Information("{Function} Demo Mode is on, simulating execution..", "Execute");
-                    
+
                 }
 
                 var cancelsubprojs = _subHelper.GetRequestCancelSubsForTre();
@@ -424,7 +426,7 @@ namespace TRE_API
                     listOfSubmissions?.Count);
                 foreach (var aSubmission in listOfSubmissions)
                 {
-                    
+
 
                     try
                     {
@@ -445,7 +447,7 @@ namespace TRE_API
                         }
                         else
                         {
-                             
+
 
 
                             // The TES message
@@ -463,7 +465,7 @@ namespace TRE_API
                                         aSubmission.Id);
                                     processedOK = false;
                                 }
-                            }   
+                            }
 
                             // **************  SEND TO RABBIT
                             if (useRabbit)
@@ -484,7 +486,7 @@ namespace TRE_API
                                     processedOK = false;
                                 }
                             }
- 
+
                             // **************  SEND TO TESK
                             if (useTESK)
                             {
@@ -525,7 +527,7 @@ namespace TRE_API
                                     tesMessage.Outputs = new List<TesOutput> { };
                                 }
 
-                               
+
 
 
 
@@ -554,22 +556,16 @@ namespace TRE_API
                                     MandatoryInput = JsonConvert.DeserializeObject<TesInput>(_AgentSettings.MandatoryInput);
                                     tesMessage.Inputs.Add(MandatoryInput);
                                 }
-                                
+
 
                                 var Files = await _minioTreHelper.GetFilesInBucket(InputBucket);
 
                                 foreach (var input in tesMessage.Inputs)
                                 {
-                                    
-                                    input.Path = input.Path.Replace("..", "");
-                                    input.Path = "/data" + input.Path;
-                                    input.Url = "s3://" + InputBucket + input.Path;
-                                    var CleanedIntput = input.Path;
 
-                                    if (input.Path.StartsWith("/"))
-                                    {
-                                        CleanedIntput = CleanedIntput.Remove(0, 1);
-                                    }
+                                    input.Path = input.Path.Replace("..", "");
+
+                                    input.Url = "s3://" + InputBucket + input.Path;
 
                                     if (string.IsNullOrEmpty(input.Name))
                                     {
@@ -588,14 +584,40 @@ namespace TRE_API
                                         }
                                     }
 
-                                    var source = await _minioSubHelper.GetCopyObject(aSubmission.Project.SubmissionBucket, CleanedIntput);
 
-                                    if (Files.S3Objects.Any(x => x.ETag == source.ETag))
+                                    var CleanedIntput = input.Path;
+                                    input.Path = "/data" + input.Path;
+                                    if (CleanedIntput.StartsWith("/"))
                                     {
-                                        continue;
+                                        CleanedIntput = CleanedIntput.Remove(0, 1);
                                     }
 
-                                    var resultcopy = await _minioTreHelper.CopyObjectToDestination(InputBucket, CleanedIntput, source);
+
+                                    var NewCleanedInput = input.Path;
+                                    if (NewCleanedInput.StartsWith("/"))
+                                    {
+                                        NewCleanedInput = NewCleanedInput.Remove(0, 1);
+                                    }
+
+
+                                    Log.Information($"getting copy for {CleanedIntput} for SubmissionBucket {aSubmission.Project.SubmissionBucket} to {NewCleanedInput}");
+
+                                    var source = await _minioSubHelper.GetCopyObject(aSubmission.Project.SubmissionBucket, CleanedIntput);
+                                    try
+                                    {
+                                        if (Files?.S3Objects != null && Files.S3Objects.Any(x => x.ETag == source.ETag))
+                                        {
+                                            continue;
+                                        }
+
+                                        var resultcopy = await _minioTreHelper.CopyObjectToDestination(InputBucket, NewCleanedInput, source);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Log.Error(ex.ToString());
+                                        throw ex;
+                                    }
+
 
                                 }
 
@@ -609,13 +631,16 @@ namespace TRE_API
                                                 _AgentSettings.ImageNameToAddToToken);
                                 foreach (var Executor in tesMessage.Executors)
                                 {
+                                    if (Executor.Env == null)
+                                    {
+                                        Executor.Env = new Dictionary<string, string>();
+                                    }
+
                                     Log.Information("Executor.Image > " + Executor.Image);
                                     if (await _features.IsEnabledAsync(FeatureFlags.SqlAndNotGraphQl))
                                     {
                                         if (Executor.Image.Contains(_AgentSettings.ImageNameToAddToToken))
                                         {
-
-
                                             Executor.Env["TRINO_SERVER_URL"] = _AgentSettings.URLTrinoToAdd;
                                             Executor.Env["ACCESS_TOKEN"] = Token;
                                             Executor.Env["USER_NAME"] = aSubmission.SubmittedBy.Name;
