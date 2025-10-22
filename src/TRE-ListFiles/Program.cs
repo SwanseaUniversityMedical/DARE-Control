5833aafb56139c3e7553ca93183354e48c4dc1d0
John.Vaughan <john.vaughan@chi.swan.ac.uk>
2025-10-22 15:20:45 +0100

    adds   / dataset

--- src/TRE-ListFiles/Program.cs
+++ src/TRE-ListFiles/Program.cs
@@ -38,7 +38,21 @@ try
             Console.WriteLine(file);
         }
     }
+    logFile = Path.Combine(logDir, "file_logdataset.txt");
+    startDirectory = "/dataset";
+    using (StreamWriter writer = new StreamWriter(logFile, false))
+    {
+        Console.WriteLine($"Scanning: {startDirectory}");
+        Console.WriteLine($"Logging to: {logFile}");
 
+        foreach (string file in Directory.EnumerateFiles(startDirectory, "*", SearchOption.AllDirectories))
+        {
+            writer.WriteLine(file);
+            Console.WriteLine(file);
+        }
+    }
+
+  
     Console.WriteLine(" Done! All file paths logged to /app/data/file_log.txt");
 }
 catch (Exception ex)
