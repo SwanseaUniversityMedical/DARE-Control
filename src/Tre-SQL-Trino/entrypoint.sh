5f8d6aeaff0851939c16256751fe1c271bf7f037
John-Vaughan <114492829+John-Vaughan@users.noreply.github.com>
2025-11-17 10:44:20 +0000

    update Tre-SQL-Trino

--- src/Tre-SQL-Trino/entrypoint.sh
+++ src/Tre-SQL-Trino/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if required environment variables are provided
-if [[ -z "$TRINO_SERVER_URL" || -z "$SQL_STATEMENT" || -z "$ACCESS_TOKEN" || -z "$USER_NAME" || -z "$SCHEMA" || -z "$CATALOG"   ]]; then
-    echo "Please provide TRINO_SERVER_URL, ACCESS_TOKEN, SCHEMA, CATALOG, SQL_STATEMENT and USER_NAME environment variables ."
+if [[ -z "$trinoURL" || -z "$SQL_STATEMENT" || -z "$trinoPassword" || -z "$trinoUsername" || -z "$SCHEMA" || -z "$CATALOG"   ]]; then
+    echo "Please provide TRINO_SERVER_URL, trinoPassword, SCHEMA, CATALOG, SQL_STATEMENT and USER_NAME environment variables ."
     exit 1
 fi
 
 
 # Execute Trino CLI command to run SQL statement and output CSV
-trino --server "$TRINO_SERVER_URL" --execute "$SQL_STATEMENT" --access-token "$ACCESS_TOKEN" --user "$USER_NAME" --schema "$SCHEMA" --catalog "$CATALOG" --insecure --output-format CSV > ~/result.csv
+trino --server "$trinoURL" --execute "$SQL_STATEMENT" --password "$trinoPassword" --user "$trinoUsername" --schema "$SCHEMA" --catalog "$CATALOG" --insecure --output-format CSV > ~/result.csv
 
 ls -la ~/
 
