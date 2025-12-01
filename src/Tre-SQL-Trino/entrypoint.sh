1cfb56619e165f2627d321e036f073e167f6ef90
John-Vaughan <114492829+John-Vaughan@users.noreply.github.com>
2025-12-01 16:03:07 +0000

    fix

--- src/Tre-SQL-Trino/entrypoint.sh
+++ src/Tre-SQL-Trino/entrypoint.sh
@@ -2,13 +2,13 @@
 
 # Check if required environment variables are provided
 if [[ -z "$trinoURL" || -z "$SQL_STATEMENT" || -z "$trinoPassword" || -z "$trinoUsername" || -z "$SCHEMA" || -z "$CATALOG"   ]]; then
-    echo "Please provide trinoURL, trinoPassword, SCHEMA, CATALOG, SQL_STATEMENT and USER_NAME environment variables ."
+    echo "Please provide trinoURL, trinoPassword, SCHEMA, CATALOG, SQL_STATEMENT and trinoUsername environment variables ."
     exit 1
 fi
 
 
 # Execute Trino CLI command to run SQL statement and output CSV
-trino --server "$trinoURL" --execute "$SQL_STATEMENT" --password "$trinoPassword" --user "$trinoUsername" --schema "$SCHEMA" --catalog "$CATALOG" --insecure --output-format CSV > ~/result.csv
+trino --server="$trinoURL" --execute="$SQL_STATEMENT" --password="$trinoPassword" --user="$trinoUsername" --schema="$SCHEMA" --catalog="$CATALOG" --insecure --output-format CSV > ~/result.csv
 
 ls -la ~/
 
