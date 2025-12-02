d1dedccf2cac23bbd8576ad79b4cd863e2ebd03b
John-Vaughan <114492829+John-Vaughan@users.noreply.github.com>
2025-12-02 11:10:12 +0000

    out

--- src/Tre-SQL-Trino/entrypoint.sh
+++ src/Tre-SQL-Trino/entrypoint.sh
@@ -10,7 +10,7 @@ fi
 export TRINO_PASSWORD="$trinoPassword"
 
 # Execute Trino CLI command to run SQL statement and output CSV
-trino --server="$trinoURL" --execute="$SQL_STATEMENT" --password="true" --user="$trinoUsername" --schema="$SCHEMA" --catalog="$CATALOG" --insecure --output-format CSV > ~/result.csv 2>&1
+trino --server="$trinoURL" --execute="$SQL_STATEMENT" --password="true" --user="$trinoUsername" --schema="$SCHEMA" --catalog="$CATALOG" --insecure --output-format CSV > /workspace/data/result.csv
 
 ls -la ~/
 
