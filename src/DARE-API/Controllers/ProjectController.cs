46e6730c441c90a74148e0aec8ca3025d5b48bd1
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2026-01-16 11:26:54 +0000

    add GetSearchData() back

--- src/DARE-API/Controllers/ProjectController.cs
+++ src/DARE-API/Controllers/ProjectController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authentication;
 using BL.Models.APISimpleTypeReturns;
 using DARE_API.Services;
 using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace DARE_API.Controllers
 {
@@ -654,5 +655,55 @@ namespace DARE_API.Controllers
                 throw;
             }
         }
+        [AllowAnonymous]
+        [HttpGet("GetSearchData")]
+        public List<Project> GetSearchData(string searchString)
+        {
+            try
+            {
+
+                //List<Project> searchResults = _DbContext.Projects
+                //    .Include(c => c.Users)
+                //    .Include(c => c.Submissions)
+                //     .Include(c => c.Tres)
+                //    .Where(c => c.Name.ToLower().Contains(searchString.Trim().ToLower()) ||
+                //    c.Users.Any(t => t.Name.ToLower().Contains(searchString.Trim().ToLower())) ||
+                //    c.Tres.Any(t => t.Name.ToLower().Contains(searchString.Trim().ToLower())) || c.Submissions.Any(s => s.TesName.Contains(searchString.Trim().ToLower()))).ToList();
+                string normalizedSearchString = $"%{searchString.Trim()}%";
+
+                List<Project> searchResults = _DbContext.Projects
+
+                    .Include(c => c.Users)
+
+                    .Include(c => c.Submissions)
+
+                    .Include(c => c.Tres)
+
+                    .Where(c => EF.Functions.Like(c.Name, normalizedSearchString) ||
+
+
+                                c.Users.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
+
+                                c.Tres.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
+
+                                c.Submissions.Any(s => EF.Functions.Like(s.TesName, normalizedSearchString))
+
+                    )
+
+                    .ToList();
+                Log.Information("{Function} Search Data retrieved successfully", "GetSearchData");
+                return searchResults.ToList();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "{Function} Crash", "GetSearchData");
+                throw;
+            }
+
+        }
+
+
+        //End
+        
     }
 }
